4d043e715271d2887ed711966fdcdb63b38897bb
maty <matikster1@wp.pl>
2022-08-01 20:08:07 +0200

    Create a new Calendar Field component

---
+++ src/components/CalendarField.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="field">{{ nrDay }}</div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "CalendarField",
+  props: {
+    nrDay: {
+      type: Number,
+      required: true,
+    },
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.field {
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  font-size: 15px;
+  font-family: sans-serif;
+}
+
+@media only screen and (max-width: 300px) {
+  .field {
+    font-size: 12px;
+  }
+}
+</style>
