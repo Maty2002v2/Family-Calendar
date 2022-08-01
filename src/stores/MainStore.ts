6c3f30fa2158915e09d1ed3abc620ab373cecba8
maty <matikster1@wp.pl>
2022-08-01 12:01:42 +0200

    Adding pinia store

---
+++ src/stores/MainStore.ts
@@ -0,0 +1,25 @@
+import { defineStore } from "pinia";
+
+export const useMainStore = defineStore("Main", {
+  state: () => {
+    return {
+      day: new Date().getDate(),
+      month: new Date().getMonth(),
+      year: new Date().getFullYear(),
+    };
+  },
+  getters: {
+    daysInMonth: (state) => new Date(state.year, state.month + 1, 0).getDate(),
+    firstMonthDay: (state) => {
+      const tempDate = new Date(state.year, state.month, 1);
+      let firstMonthDay = tempDate.getDay();
+      
+      if (firstMonthDay === 0) {
+        firstMonthDay = 7;
+      }
+
+      return firstMonthDay;
+    },
+  },
+  actions: {},
+});
