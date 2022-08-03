a674e5d04455069f398f50b8c3c3947fa4dc795e
maty <matikster1@wp.pl>
2022-08-03 18:14:02 +0200

    Creating an api - creating a calendar.php class

---
+++ family-calendar-api/classes/calendar.php
@@ -0,0 +1,47 @@
+<?php
+require_once('database.php');
+
+class Calendar {
+    private $id;
+
+    public function __construct($id = "")
+    {   
+        $this->id = $id;
+    }
+
+    public function create_calendar(): array {
+        $db = Database::getInstance();
+        $hash = $this->create_hash();
+        $sql = "INSERT INTO calendars (id) VALUES ('".$hash."')";
+
+        if($db->query($sql)) {
+            return ["is_ok" => true, "hash" => $hash];
+        } else {
+            return ["is_ok" => false];
+        }
+
+    }
+
+    public function does_it_exist(): bool {
+        $db = Database::getInstance();
+        $sql = "SELECT * FROM calendars WHERE id = '".$this->id."' LIMIT 1";
+
+        if($db->rows($db->query($sql)) === 1) {
+            return true;
+        } 
+
+        return false;
+    }
+
+    private function create_hash($length = 7) {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+
+         return $randomString;
+    }
+}
