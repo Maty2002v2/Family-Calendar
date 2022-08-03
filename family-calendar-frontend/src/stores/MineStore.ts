f259cc3739d0a7f73c8a4d58fd4187027fde6bb1
maty <matikster1@wp.pl>
2022-08-03 20:27:37 +0200

    Dodanie funkcji tworzącej nowy kalendarz

--- family-calendar-frontend/src/stores/MineStore.ts
+++ family-calendar-frontend/src/stores/MineStore.ts
@@ -17,6 +17,25 @@ export const useMainStore = defineStore("Mine", {
     switchIncorrectCodeEntered(value: boolean) {
       this.incorrectCodeEntered = value;
     },
+    async createCalendar() {
+      const result = { error: false, message: "" };
+
+      await fetch("http://localhost/family-calendar-api?action=add-calendar", {
+        method: "GET",
+        mode: "cors",
+      })
+        .then((response) => response.json())
+        .then((response) => {
+          result.error = response.error;
+          result.message = response.message;
+        })
+        .catch((error) => {
+          result.error = true;
+          result.message = error;
+        });
+
+      return result;
+    },
     async checkIfThereIsCalendar(calendar_id: string) {
       const url = new URL("http://localhost/family-calendar-api");
       const params = { action: "exist-calendar", calendar_id: calendar_id };
