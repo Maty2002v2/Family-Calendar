f150d6c07034848b16a88483efc63d001edf8f97
maty <matikster1@wp.pl>
2022-08-04 20:37:44 +0200

    Create a new type for the day

---
+++ family-calendar-frontend/src/types/DayObject.ts
@@ -0,0 +1,13 @@
+type DayObject = {
+  id?: number;
+  calendarId?: number;
+  numberDay?: number;
+  numberMonth?: number;
+  numberYear?: number;
+  toRepeat?: boolean;
+  title?: string;
+  description?: string;
+  categoryDay?: number;
+};
+
+export default DayObject;
