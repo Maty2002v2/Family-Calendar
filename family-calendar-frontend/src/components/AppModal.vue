f71b25d6a0db354e92ed6cf61a27d1d54e57ea41
maty <matikster1@wp.pl>
2022-08-16 16:34:34 +0200

    Create a new AppModal component that will be a template for models in the application

---
+++ family-calendar-frontend/src/components/AppModal.vue
@@ -0,0 +1,58 @@
+<template>
+  <Transition
+    enter-active-class="animate__animated animate__faster  animate__fadeIn"
+    leave-active-class="animate__animated animate__faster animate__fadeOut"
+  >
+    <div class="app-modal" v-show="isShow" @click.self="closeModal">
+      <Transition
+        enter-active-class="animate__animated animate__faster animate__backInDown"
+        leave-active-class="animate__animated animate__faster animate__fadeOut"
+      >
+        <div v-show="isShow" class="app-modal__slot">
+          <slot></slot>
+        </div>
+      </Transition>
+    </div>
+  </Transition>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "AppModal",
+  props: {
+    isShow: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  emits: ["closeModal"],
+  setup(props, { emit }) {
+    const closeModal = () => {
+      emit("closeModal");
+    };
+
+    return { closeModal };
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.app-modal {
+  @include position($top: 0px, $left: 0px);
+
+  width: 100vw;
+  min-height: 100%;
+  padding-top: 10vh;
+  padding-bottom: 10vh;
+
+  background: rgba($color: #000000, $alpha: 0.3);
+
+  &__slot {
+    @include flexbox;
+    @include justify-content(center);
+  }
+}
+</style>
