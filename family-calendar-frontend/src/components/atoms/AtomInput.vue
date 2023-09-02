d3ad4931dfb52373ca66628e04f62112d843e877
Maty2002v2 <matikster21@gmail.com>
2023-09-02 22:37:32 +0200

    Created AtomInput comonent

---
+++ family-calendar-frontend/src/components/atoms/AtomInput.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <input 
+      :type="type" 
+      :value="value"
+      class="atom-input" 
+      @input="$emit('update:modelValue', $event.target.value)"
+    >
+  </div>
+</template>
+<script>
+export default {
+  name: "AtomInput",
+  props: {
+    modelValue: {
+      type: String,
+      default: '',
+    },
+    type: {
+      type: String,
+      default: 'text',
+      validator(value) {
+        return ['text', 'email', 'tel', 'number'].includes(value);
+      },
+    },
+    required: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
+<style lang="scss">
+.atom-input {
+  width: 100%;
+  line-height: 1.5;
+  padding: 10px;
+  border-radius: 5px;
+  border: 1px solid #ccc;
+  box-sizing: border-box;
+
+  color: $color-day-field;
+  font-size: 15px;
+  letter-spacing: 1px;
+
+  transition: all 0.3s ease;
+
+  &:focus {
+    outline-color: $active-day;
+    box-shadow: 0px 0px 3px $active-day;
+  }
+}
+</style>
