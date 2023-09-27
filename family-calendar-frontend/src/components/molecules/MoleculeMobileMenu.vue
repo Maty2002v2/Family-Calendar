de86600a6132c1e4d9eaccd1a6a19288d5d85982
Maty2002v2 <matikster21@gmail.com>
2023-09-27 21:21:35 +0200

    Changing MoleculeMobileMenu component (adding transparent 'border' and changing button color after click)

--- family-calendar-frontend/src/components/molecules/MoleculeMobileMenu.vue
+++ family-calendar-frontend/src/components/molecules/MoleculeMobileMenu.vue
@@ -1,14 +1,31 @@
 <template>
   <div class="molecule-mobile-menu">
-    <button class="molecule-mobile-menu__triger"></button>
+    <button class="molecule-mobile-menu__triger" @click="switchState" ref="trigerButton"></button>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
 
 export default defineComponent({
   name: "MoleculeMobileMenu",
+  setup() {
+    const trigerButton = ref<HTMLDivElement>();
+
+    const switchState = () => {
+      if(!trigerButton.value) return;
+
+      trigerButton.value.classList.toggle("is-rotate");
+      // for (var i = 0; i < item.length; i++) {
+      //     item[i].classList.toggle(`item-${i}`);
+      // }
+    }
+
+    return {
+      trigerButton,
+      switchState
+    }
+  }
 });
 </script>
 
@@ -21,21 +38,27 @@ export default defineComponent({
   padding: 30px;
   box-sizing: border-box;
   box-shadow: rgba(17, 17, 26, 0.1) 0px 0px 16px;
-  background: #FFFFFF;
+
+  background: radial-gradient(circle at top, rgba(238, 238, 238, 0) 0%, rgba(238, 238, 238, 0) 28%, rgb(238, 238, 238) 27%, rgb(238, 238, 238) 100%);
 
   &__triger {
     position: absolute;
-    bottom: 80px;
-    left: calc(50% - 50px);
-    width: 100px;
-    height: 100px;
+    bottom: 80px;//use position
+    left: calc(50% - 40px);
+    width: 80px;
+    height: 80px;
     background-color: #008EFA;
-    border: 10px solid #EEEEEE;
+    border: none;
     border-radius: 50%;
     padding: 20px;
     cursor: pointer;
     transform: translateY(50%);
     transition: .35s ease;
+  }
+
+  .is-rotate {
+    transform: translateY(40%) rotateZ(225deg);
+    background-color: #FF207A;
   }
 }
 </style>
