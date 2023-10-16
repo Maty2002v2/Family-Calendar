5053c25f67f8d5102f9a6c41d1357a5b25f9a88a
Maty2002v2 <matikster21@gmail.com>
2023-10-16 21:28:57 +0200

    Created useNotifications composable, MoleculeNotificationsWrapper component, MoleculeNotification component (basic files for adding notifications)

---
+++ family-calendar-frontend/src/components/molecules/MoleculeNotification.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="molecule-notification">
+    elo
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: "MoleculeNotification"
+})
+</script>
+
+<style lang="scss">
+.molecule-notification {
+  width: 100px;
+  background-color: white;
+}
+</style>
---
+++ family-calendar-frontend/src/components/molecules/MoleculeNotificationsWrapper.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="molecule-notifications-wrapper">
+    elo
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+import MoleculeNotification from "@/components/molecules/MoleculeNotification.vue";
+
+export default defineComponent({
+  name: "MoleculeNotificationsWrapper",
+  components: {
+    MoleculeNotification,
+  }
+})
+</script>
+
+<style lang="scss">
+.molecule-notifications-wrapper {
+  width: 100px;
+  background-color: white;
+}
+</style>
