5197f48d5e0f32b906d4a4d6a7ec1f4c008aa2e4
Maty2002v2 <matikster21@gmail.com>
2023-10-21 11:03:52 +0200

    Modification code (move months names and week days names only to translations, and transltion use only)

--- family-calendar-frontend/src/components/molecules/Calendar/MoleculeNamesDaysOfWeek.vue
+++ family-calendar-frontend/src/components/molecules/Calendar/MoleculeNamesDaysOfWeek.vue
@@ -1,34 +1,29 @@
 <template>
   <div class="molecule-names-days-of-week">
     <div
-      v-for="(name, index) in namesDaysOfWeek"
+      v-for="(index) in 7"
       :key="index"
       class="molecule-names-days-of-week__div"
       :class="{
-        'molecule-names-days-of-week__div--sunday': name == namesDaysOfWeek[0],
+        'molecule-names-days-of-week__div--sunday': index == 1,
       }"
     >
-      {{ t(`shortNamesDaysOfWeek.${name}`) }}
+      {{ t(`shortNamesDaysOfWeek.${index}`) }}
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import { storeToRefs } from 'pinia';
 import { useI18n } from 'vue-i18n';
-
-import { useDateStore } from "@/stores/DateStore";
 
 export default defineComponent({
   name: "MoleculeNamesDaysOfWeek",
   setup() {
     const { t } = useI18n();
-    const { getNamesDaysOfWeek: namesDaysOfWeek } = storeToRefs(useDateStore());
 
     return { 
-      t,
-      namesDaysOfWeek,
+      t
     };
   },
 });
