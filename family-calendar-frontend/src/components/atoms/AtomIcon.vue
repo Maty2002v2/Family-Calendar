843e0242c45c39825974be167318ea2d9f32d9df
Maty2002v2 <matikster21@gmail.com>
2023-11-15 19:07:02 +0100

    Modification AtomIcon component (change method setup to script setup)

--- family-calendar-frontend/src/components/atoms/AtomIcon.vue
+++ family-calendar-frontend/src/components/atoms/AtomIcon.vue
@@ -1,11 +1,3 @@
 <template>
   <i class="demo-icon"></i>
 </template>
-
-<script lang="ts">
-import { defineComponent } from "vue";
-
-export default defineComponent({
-  name: "AtomIcon"
-});
-</script>
